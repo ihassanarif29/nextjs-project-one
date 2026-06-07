9c363bfae5ba89c9686f9393119cf81b52509ad2
Hassan Arif <ihassanarif29@gamil.com>
2026-06-07 07:40:02 +0500

    Add StickyScrollSection and StickyScroll components, implement scrolling effects, and enhance layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCources from "@/components/FeaturedCources";
 import HeroSection from "@/components/HeroSection";
+import StickyScrollSection from "@/components/StickyScrollSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/96 antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCources />
+      <StickyScrollSection />
     </main>
   );
 }
